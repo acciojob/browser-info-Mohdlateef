bcc23990fb02d00c726460db7588cf9adae68df9
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-28 22:58:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+//your JS code here. If requic
+const addelement=document.getElementByTagName("body");
+const newelement=document.createElement("div");
+newelement.innerText="abc";
+addelement.appendChild(newelement);
