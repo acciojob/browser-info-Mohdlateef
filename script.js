155b314d84d84d3d082f3fce89b1957a2bb0cec6
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-28 23:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If requic
 const addelement=document.querySelector("body");
 const newelement=document.createElement("div");
-newelement.innerText=navigator.userAgent;
+newelement.innerText="your are using version"+navigator.userAgent;
 addelement.appendChild(newelement);
 // alert("abc");
